f4740fe65a3baeac40334cf0af72d3d5c6f047f6
Cerberuser <computers05@mail.ru>
2019-10-23 12:47:36 +0300

    Started writing macros and types

--- dependent/src/lib.rs
+++ dependent/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![feature(proc_macro_hygiene)]
+
+use typenum::Unsigned;
+use std::marker::PhantomData;
+use dependent_attribute::*;
+
+pub struct Vect<Item, Size>(Vec<Item>, PhantomData<Size>);
+
+pub trait DependentVec<Item> {
+    fn try_as_fixed<Size: Unsigned>(self) -> Option<Vect<Item, Size>>;
+}
+
+#[dependent_out]
+pub fn collect_from<Item>(iter: impl Iterator<Item = Item>) -> Vec<Item> {
+    iter.collect()
 }
--- dependent_attribute/src/lib.rs
+++ dependent_attribute/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use proc_macro2::TokenStream as pm2_ts;
+
+#[proc_macro_attribute]
+pub fn dependent_out(input: TokenStream, _: TokenStream) -> TokenStream {
+    let input = pm2_ts::from(input);
+    let output = quote! {
+        dependent_out_inner! { #input }
+    };
+    output.into()
+}
+
+#[proc_macro]
+pub fn dependent_out_inner(input: TokenStream) -> TokenStream {
+    input
+}
+
+#[proc_macro_attribute]
+pub fn dependent_check(input: TokenStream, _: TokenStream) -> TokenStream {
+    input
 }
