40b9c1cdc06475db9b8442dc3606eefd39914a22
Cerberuser <computers05@mail.ru>
2019-10-24 09:08:52 +0300

    Implemented draft for DependentVec

--- dependent/src/lib.rs
+++ dependent/src/lib.rs
@@ -1,16 +1,59 @@
 #![feature(proc_macro_hygiene)]
 
-use typenum::Unsigned;
-use std::marker::PhantomData;
 use dependent_attribute::*;
+use std::ops::Add;
+use std::str::FromStr;
 
-pub struct Vect<Item, Size>(Vec<Item>, PhantomData<Size>);
-
-pub trait DependentVec<Item> {
-    fn try_as_fixed<Size: Unsigned>(self) -> Option<Vect<Item, Size>>;
+pub trait DependentVec<Inner>: Sized {
+    fn try_unify<T: DependentVec<Inner>>(self, other: T) -> Result<(Self, Self), (Self, T)>;
+    fn len(&self) -> usize;
+    fn into_inner(self) -> Inner;
+    fn map2(self, other: Self, f: impl FnOnce(Inner, Inner) -> Inner) -> Self;
 }
 
-#[dependent_out]
-pub fn collect_from<Item>(iter: impl Iterator<Item = Item>) -> Vec<Item> {
-    iter.collect()
+pub struct Vect<Item>(Vec<Item>);
+impl<Item> DependentVec<Vec<Item>> for Vect<Item> {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+    fn into_inner(self) -> Vec<Item> {
+        self.0
+    }
+    fn try_unify<T: DependentVec<Vec<Item>>>(self, other: T) -> Result<(Self, Self), (Self, T)> {
+        if self.len() == other.len() {
+            let other = Self(other.into_inner());
+            Ok((self, other))
+        } else {
+            Err((self, other))
+        }
+    }
+    fn map2(self, other: Self, f: impl FnOnce(Vec<Item>, Vec<Item>) -> Vec<Item>) -> Self {
+        Self(f(self.into_inner(), other.into_inner()))
+    }
 }
+
+impl<Item> From<Vec<Item>> for Vect<Item> {
+    fn from(input: Vec<Item>) -> Self {
+        Self(input)
+    }
+}
+
+#[dependent_out(DependentVec<Vec<Item>>, Vect<Item>)]
+pub fn parse_list_discarding<Item: FromStr>(input: String) -> Vec<Item> {
+    input
+        .split(",")
+        .map(str::parse)
+        .filter_map(Result::ok)
+        .collect()
+}
+
+pub fn zip_add<Item: Add<Item, Output = Item>, T: DependentVec<Vec<Item>>>(first: T, second: T) -> T {
+    first.map2(second, |v1, v2| v1.into_iter().zip(v2.into_iter()).map(|(i1, i2)| i1 + i2).collect())
+}
+
+#[test]
+fn test() {
+    let v1 = parse_list_discarding!([i64] "1,2,3,str,4".into());
+    let v2 = parse_list_discarding!([i64] "-1,-2,str,-3,-4".into());
+    let (v1, v2) = v1.try_unify(v2).unwrap_or_else(|_| panic!("Not equal length"));
+    assert_eq!(zip_add(v1, v2).into_inner(), vec![0, 0, 0, 0]);}
--- dependent_attribute/src/lib.rs
+++ dependent_attribute/src/lib.rs
@@ -1,24 +1,67 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use proc_macro2::TokenStream as pm2_ts;
+use syn::*;
 
 #[proc_macro_attribute]
-pub fn dependent_out(input: TokenStream, _: TokenStream) -> TokenStream {
-    let input = pm2_ts::from(input);
+pub fn dependent_out(attr: TokenStream, input: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(input as ItemFn);
+    let Params { trait_path, struct_path } = parse_macro_input!(attr as Params);
+
+    let orig_out = match input.sig.output {
+        ReturnType::Type(_, ty) => *ty.clone(),
+        _ => {
+            return Error::new(
+                Span::call_site(),
+                "Function with unit return type can't be dependently-typed",
+            )
+            .to_compile_error()
+            .into()
+        }
+    };
+    input.sig.output = parse2(quote! { -> impl #trait_path }).unwrap();
+
+    let name = input.sig.ident.clone();
+
+    let params = input.sig.generics.clone();
+    let (generic_pat, generic_out) = if params.lt_token.is_some() {
+        // We need to generate generic parameters
+        let pat = quote! { [$($param:tt)*] };
+        let out = quote! { ::<$($param)*> };
+        (pat, out)
+    } else {
+        (quote! {}, quote! {})
+    };
+
+    let body = input.block.clone();
+    input.block = parse2(quote! {{ let out: #orig_out = #body; let out: #struct_path = out.into(); out }}).unwrap();
+
     let output = quote! {
-        dependent_out_inner! { #input }
+        macro_rules! #name {
+            (#generic_pat $($input:tt)*) => {{
+                #input
+                #name#generic_out($($input)*)
+            }};
+            // This pattern will be matched only if the generics are required but not provided.
+            // If they are not required, the first pattern will always match.
+            ($($input:tt)*) => { compile_error!("Function with dependent output cannot infer its generic parameters") };
+        }
     };
     output.into()
 }
 
-#[proc_macro]
-pub fn dependent_out_inner(input: TokenStream) -> TokenStream {
-    input
+struct Params {
+    trait_path: TypePath,
+    struct_path: TypePath,
 }
 
-#[proc_macro_attribute]
-pub fn dependent_check(input: TokenStream, _: TokenStream) -> TokenStream {
-    input
+impl parse::Parse for Params {
+    fn parse(input: parse::ParseStream) -> Result<Self> {
+        let trait_path = input.parse()?;
+        input.parse::<Token![,]>()?;
+        let struct_path = input.parse()?;
+        Ok(Self { trait_path, struct_path })
+    }
 }
