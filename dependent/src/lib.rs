d6a83371d8047aa0fe868aa768c945833c3440a6
k.bagrov <k.bagrov@g.nsu.ru>
2019-10-31 22:40:34 +0700

    [WIP] Second attempt

--- dependent/src/lib.rs
+++ dependent/src/lib.rs
@@ -1,72 +1,114 @@
-use dependent_attribute::dependent_trait;
-
-pub trait DependentInner: Sized {
-    type Frozen: ?Sized;
-    fn freeze(&mut self) -> &mut Self::Frozen;
-    fn recreate(_: &Self::Frozen) -> Self;
-}
-pub trait DependentInnerOperate: DependentInner {
-    fn transform(mut self, f: impl FnOnce(&mut Self::Frozen)) -> Self {
-        let mut tmp = Self::recreate(self.freeze());
-        f(tmp.freeze());
-        tmp
+pub mod traits {
+    use std::marker::PhantomData;
+    
+    pub trait DependentInner: Sized + Clone {
+        type Frozen: ?Sized;
+        fn freeze(&mut self) -> &mut Self::Frozen;
+        fn recreate(_: &Self::Frozen) -> Self;
     }
-    fn operate(&mut self, f: impl FnOnce(&mut Self::Frozen)) {
-        f(self.freeze());
-    }
-}
-impl<T: DependentInner> DependentInnerOperate for T {}
-impl<Item: Clone> DependentInner for Vec<Item> {
-    type Frozen = [Item];
-    fn freeze(&mut self) -> &mut Self::Frozen {
-        self.as_mut_slice()
-    }
-    fn recreate(frozen: &Self::Frozen) -> Self {
-        frozen.iter().cloned().collect()
-    }
-}
-
-pub struct Vect<Item>(Vec<Item>);
-#[dependent_trait]
-impl<Item: Clone> DependentVec for Vect<Item> {
-    type Inner = Vec<Item>;
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-    fn into_inner(self) -> Inner {
-        self.0
-    }
-    fn inner(&self) -> &Inner {
-        &self.0
-    }
-    fn try_unify<T: Trait>(self, other: T) -> Result<(Self, Self), (Self, T)> {
-        if self.len() == other.len() {
-            let other = Self(other.into_inner());
-            Ok((self, other))
-        } else {
-            Err((self, other))
+    pub trait DependentInnerOperate: DependentInner {
+        fn transform(mut self, f: impl FnOnce(&mut Self::Frozen)) -> Self {
+            let mut tmp = Self::recreate(self.freeze());
+            f(tmp.freeze());
+            tmp
+        }
+        fn modify(&mut self, f: impl FnOnce(&mut Self::Frozen)) {
+            f(self.freeze());
         }
     }
-    fn map(self, f: impl FnOnce(&mut Inner::Frozen)) -> Self {
-        let mut inner = self.into_inner();
-        f(inner.freeze());
-        Self(inner)
+    impl<T: DependentInner> DependentInnerOperate for T {}
+
+    pub trait Dependent {
+        type Inner: DependentInnerOperate;
+        fn from_inner(_: Self::Inner) -> Self
+        where
+            Self: Sized;
+        fn into_inner(self) -> Self::Inner;
+        fn inner(&self) -> &Self::Inner;
+        fn map(self, f: impl FnOnce(&mut <Self::Inner as DependentInner>::Frozen)) -> Self
+        where
+            Self: Sized,
+        {
+            let mut inner = self.into_inner();
+            f(inner.freeze());
+            Self::from_inner(inner)
+        }
+        fn consume(self, f: impl FnOnce(&mut <Self::Inner as DependentInner>::Frozen))
+        where
+            Self: Sized,
+        {
+            f(self.into_inner().freeze())
+        }
+        fn map_ref(&self, f: impl FnOnce(&mut <Self::Inner as DependentInner>::Frozen)) -> Self
+        where
+            Self: Sized,
+        {
+            let mut inner: Self::Inner = self.inner().clone();
+            f(inner.freeze());
+            Self::from_inner(inner)
+        }
+        fn consume_ref(&self, f: impl FnOnce(&mut <Self::Inner as DependentInner>::Frozen)) {
+            f(self.inner().clone().freeze())
+        }
     }
-    fn consume(self, f: impl FnOnce(&mut Inner::Frozen)) {
-        f(self.into_inner().freeze())
+
+    pub struct Equiv<T1, T2>(PhantomData<(T1, T2)>);
+
+    pub trait Nat: Sized {
+        fn as_usize(&self) -> usize;
+        fn from_usize(s: usize) -> Self;
     }
-    fn map_ref(&self, f: impl FnOnce(&mut Inner::Frozen)) -> Self {
-        let mut inner = self.inner().clone();
-        f(inner.freeze());
-        Self(inner)
+    pub trait NatEq: Nat {
+        fn eq<N: Nat>(this: &Self, other: &N) -> Option<Equiv<Self, N>> {
+            if this.as_usize() == other.as_usize() {
+                Some(Equiv(PhantomData))
+            } else {
+                None
+            }
+        }
     }
-    fn consume_ref(&self, f: impl FnOnce(&mut Inner::Frozen)) {
-        f(self.inner().clone().freeze())
-    }
+    impl<T: Nat> NatEq for T {}
 }
 
-impl<Item> From<Vec<Item>> for Vect<Item> {
-    fn from(input: Vec<Item>) -> Self {
-        Self(input)
+pub mod vect {
+    use super::traits::*;
+    use core::iter::FromIterator;
+    use std::marker::PhantomData;
+    
+    impl<Item: Clone> DependentInner for Vec<Item> {
+        type Frozen = [Item];
+        fn freeze(&mut self) -> &mut Self::Frozen {
+            self.as_mut_slice()
+        }
+        fn recreate(frozen: &Self::Frozen) -> Self {
+            frozen.iter().cloned().collect()
+        }
+    }
+
+    pub struct Vect<T, N: Nat>(Vec<T>, PhantomData<N>);
+    impl<Item: Clone, N: Nat> Dependent for Vect<Item, N> {
+        type Inner = Vec<Item>;
+        fn from_inner(v: Self::Inner) -> Self {
+            Self(v, PhantomData)
+        }
+        fn into_inner(self) -> Self::Inner {
+            self.0
+        }
+        fn inner(&self) -> &Self::Inner {
+            &self.0
+        }
+    }
+    
+    impl<Item: Clone, N: Nat> FromIterator<Item> for Vect<Item, N> {
+        fn from_iter<I: IntoIterator<Item = Item>>(iter: I) -> Self {
+            Self(iter.into_iter().collect(), PhantomData)
+        }
+    }
+    
+    pub fn collect<Item: Clone, N: Nat, I: IntoIterator<Item = Item>>(
+        iter: I,
+    ) -> (N, Vect<Item, N>) {
+        let inner: Vec<_> = iter.into_iter().collect();
+        (N::from_usize(inner.len()), Vect(inner, PhantomData))
     }
 }
--- dependent/tests/test.rs
+++ dependent/tests/test.rs
@@ -1,56 +1,43 @@
-#![feature(proc_macro_hygiene)]
 
-use dependent::{DependentVec, Vect};
-use dependent_attribute::dependent_out;
-use std::ops::AddAssign;
-use std::str::FromStr;
+use dependent::traits::{Dependent, Equiv, Nat, NatEq};
+use dependent::vect::{collect, Vect};
 
-#[dependent_out(DependentVec<Vec<Item>>, Vect<Item>)]
-pub fn parse_list_discarding<Item: FromStr + Clone>(input: String) -> Vec<Item> {
-    input
-        .split(",")
-        .map(str::parse)
-        .filter_map(Result::ok)
-        .collect()
-}
-
-pub fn zip_add<Item: AddAssign<Item> + Clone, T: DependentVec<Vec<Item>>>(
-    first: &T,
-    second: &T,
-) -> T {
+fn zip_use_second<T: Clone, N1: Nat, N2: Nat>(
+    first: Vect<T, N1>,
+    second: Vect<T, N2>,
+    _proof: Equiv<N1, N2>,
+) -> Vect<T, N1> {
     first.map_ref(|v1| {
         second.consume_ref(|v2| {
             v1.iter_mut()
                 .zip(v2.iter())
-                .for_each(|(i1, i2)| *i1 += i2.clone())
+                .for_each(|(i1, i2)| *i1 = i2.clone())
         })
     })
 }
 
-#[test]
-fn test() {
-    let v1 = parse_list_discarding!(<i64> "1,2,3,str,4".into());
-    let v2 = parse_list_discarding!(<i64> "-1,-2,str,-3,-4".into());
-    let (v1, v2) = v1
-        .try_unify(v2)
-        .unwrap_or_else(|_| panic!("Not equal length"));
-    assert_eq!(zip_add(&v1, &v2).into_inner(), vec![0, 0, 0, 0]);
+macro_rules! vect {
+    ($v:expr => $n:ident) => {{
+        struct $n(usize);
+        impl Nat for $n {
+            fn as_usize(&self) -> usize {
+                self.0
+            }
+            fn from_usize(s: usize) -> Self {
+                Self(s)
+            }
+        }
+        collect::<_, $n, _>($v)
+    }}
 }
 
 #[test]
-fn error() {
-    let v1 = parse_list_discarding!(<i64> "1".into());
-    let v2 = parse_list_discarding!(<i64> "".into());
-    v1.try_unify(v2).err().unwrap();
-}
+fn equal() {
+    let (n1, v1) = vect!(vec![0] => N1);
+    let (n2, v2) = vect!(vec![1] => N2);
 
-#[test]
-fn chain() {
-    let v1 = parse_list_discarding!(<i64> "1,2,3,str,4".into());
-    let v2 = parse_list_discarding!(<i64> "-1,-2,str,-3,-4".into());
-    let (v1, v2) = v1
-        .try_unify(v2)
-        .unwrap_or_else(|_| panic!("Not equal length"));
-    let v3 = zip_add(&v1, &v2);
-    assert_eq!(zip_add(&v1, &v3).inner(), v1.inner());
+    assert_eq!(
+        NatEq::eq(&n1, &n2).map(|proof| zip_use_second(v1, v2, proof).into_inner()),
+        Some(vec![1])
+    );
 }
