f4f7b1fe9675d598b61608f972bd176c56df99fa
k.bagrov <k.bagrov@g.nsu.ru>
2019-11-28 20:02:27 +0700

    Removed `nat::ops::Less` - there's no use-case to illustrate it yet

--- dependent_nat/src/fin.rs
+++ dependent_nat/src/fin.rs
@@ -15,10 +15,6 @@ impl<N: Nat> Fin<N> {
     pub fn as_usize(self) -> usize {
         self.0
     }
-    #[cfg(feature = "nat_ops")]
-    pub fn from_proof<N2: Nat>(_proof: crate::Less<N2, N>) -> Option<Self> {
-        N2::get_usize().and_then(Self::from_usize)
-    }
 }
 
 pub struct IterUntil<N: Nat>(usize, PhantomData<N>);
