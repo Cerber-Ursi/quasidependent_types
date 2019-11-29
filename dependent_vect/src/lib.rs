9232c610b51517a4fb860a45e3c2393aed396f6d
Cerberuser <computers05@mail.ru>
2019-11-29 06:55:02 +0300

    Reducing test case

--- dependent_vect/src/lib.rs
+++ dependent_vect/src/lib.rs
@@ -1,25 +1,8 @@
-use dependent::*;
 use dependent_nat::*;
 use std::marker::PhantomData;
 
 #[derive(Clone)]
 pub struct Vect<T, N: Nat>(Vec<T>, PhantomData<N>);
-impl<Item: Clone, N: Nat> Dependent for Vect<Item, N> {
-    type Native = Vec<Item>;
-    type Frozen = [Item];
-    fn freeze(&self) -> &Self::Frozen {
-        self.0.as_slice()
-    }
-    fn freeze_mut(&mut self) -> &mut Self::Frozen {
-        self.0.as_mut_slice()
-    }
-    fn into_native(self) -> Self::Native {
-        self.0
-    }
-    fn as_native(&self) -> &Self::Native {
-        &self.0
-    }
-}
 
 pub fn collect<Item: Clone, N: Nat, N2: Nat + From<N>, I: IntoIterator<Item = Item>>(
     iter: I,
