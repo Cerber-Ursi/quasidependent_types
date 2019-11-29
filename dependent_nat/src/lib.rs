4231fa68420869fa2146794788085b75ec94bc05
Cerberuser <computers05@mail.ru>
2019-11-29 07:18:04 +0300

    Reducing test case

--- dependent_nat/src/lib.rs
+++ dependent_nat/src/lib.rs
@@ -1,10 +1,39 @@
 #![feature(proc_macro_hygiene)]
 
-mod timestamped;
-
 pub fn expect_nat<N: Nat>(s: usize) -> N {
     N::from_usize(s).unwrap()
 }
 
-pub use self::timestamped::nat::*;
+pub trait Nat: Sized + Clone + Copy {
+    fn get_usize() -> Option<usize>;
+    fn as_usize(self) -> usize;
+    fn from_usize(s: usize) -> Result<Self, ()>;
+    fn get() -> Self;
+    fn try_get() -> Option<Self>;
+}
 
+#[macro_export]
+macro_rules! with_n {
+        ($($inner:tt)*) => {{
+            #[derive(Copy, Clone)]
+            struct N;
+            impl $crate::Nat for N {
+                fn get_usize() -> Option<usize> {
+                    Some(0)
+                }
+                fn as_usize(self) -> usize {
+                    0
+                }
+                fn from_usize(s: usize) -> Result<Self, ()> {
+                    Ok(Self)
+                }
+                fn get() -> Self {
+                    Self
+                }
+                fn try_get() -> Option<Self> {
+                    Some(Self)
+                }
+            }
+            $($inner)*
+        }};
+    }
